5ea56d990d39d082201f9e277973ae79378fdb5d
freetimezp <kraftukrnet@ukr.net>
2023-07-09 00:57:01 +0300

    02. js-portfolio-23-3d. ThreeAnimation Component. Show logo animate with fog effect.

--- src/components/ThreeAnimation.jsx
+++ src/components/ThreeAnimation.jsx
@@ -1,10 +1,63 @@
 import React from "react";
 
-const CanvasAnimation = () => {
-    return (
-        <div>
+import * as THREE from 'three';
+import { Canvas, useLoader, extend } from "@react-three/fiber";
+import {
+    OrbitControls,
+    Edges,
+    OrthographicCamera,
+    Sparkles,
+    Cloud,
+    Effects
+} from '@react-three/drei';
+import { UnrealBloomPass } from "three-stdlib";
 
-        </div>
+import Logo from "@images/logo.jpg";
+
+extend({ UnrealBloomPass });
+
+const CanvasAnimation = () => {
+    const repeatX = 1;
+    const repeatY = 1;
+    const base = useLoader(THREE.TextureLoader, Logo);
+    base.wrapS = THREE.RepeatWrapping;
+    base.wrapT = THREE.RepeatWrapping;
+    base.repeat.set(repeatX, repeatY);
+
+    return (
+        <Canvas shadow="true" gl={{ antialias: false }}>
+            <color attach="background" args={["#202030"]} />
+
+            <mesh scale={1.5}>
+                <boxGeometry />
+                <meshPhysicalMaterial map={base} />
+                <Edges
+                    scale={1.1}
+                    threshold={2}
+                    color="#000000"
+                />
+            </mesh>
+
+            <directionalLight intensity={1} position={[10, 10, 10]} />
+            <OrbitControls autoRotate enableZoom={false} />
+            <OrthographicCamera makeDefault far={500} near={0.1} position={[-10, 2, -10]} zoom={100} />
+            <hemisphereLight intensity={1} color={"red"} groundColor={"blue"} />
+
+            <Sparkles count={200} scale={[20, 20, 20]} size={1} speed={0.01} />
+
+            <fog attach={"fog"} args={["#202030", 5, 25]} />
+            <Cloud
+                opacity={0.1}
+                speed={0.1}
+                width={30}
+                depth={0.1}
+                segments={40}
+            />
+
+            <Effects disableGamma>
+                <unrealBloomPass threshold={1} strength={2.0} radius={0.5} />
+            </Effects>
+        </Canvas>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import CanvasAnimation from '../components/ThreeAnimation';
 
 const IndexPage = () => {
   return (
     <main>
-      IndexPage
+      <CanvasAnimation />
+
+      <div className="header">
+        <h1>PORTFOLIO</h1>
+        <h3>By: Freetime</h3>
+      </div>
     </main>
   )
 }
 
-
 export default IndexPage;
+
+export const Head = () => {
+  return (
+    <title>
+      Portfolio - 3D
+    </title>
+  )
+}
 
